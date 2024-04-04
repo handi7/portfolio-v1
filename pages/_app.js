3eddb097d689a4311ac79c7706dde6a7ea59d385
handi7 <handiani@titip.io>
2024-04-04 12:41:21 +0700

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,12 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "react-toastify/dist/ReactToastify.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Analytics />
       <Component {...pageProps} />;
     </Layout>
   );
